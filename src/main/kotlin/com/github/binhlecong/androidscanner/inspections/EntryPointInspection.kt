a92e26d8939fa1e936657a64d035ccabf9f6aeda
binhlecong <congbinh21012001@gmail.com>
2022-11-02 14:16:51 +0700

    demo get argument at location, pass visitor instead of lambda object

--- src/main/kotlin/com/github/binhlecong/androidscanner/inspections/EntryPointInspection.kt
+++ src/main/kotlin/com/github/binhlecong/androidscanner/inspections/EntryPointInspection.kt
@@ -1,10 +1,12 @@
 package com.github.binhlecong.androidscanner.inspections
 
 import com.github.binhlecong.androidscanner.visitors.CallExpressionVisitor
-import com.intellij.codeInspection.*
+import com.intellij.codeInspection.AbstractBaseUastLocalInspectionTool
+import com.intellij.codeInspection.InspectionManager
+import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.psi.PsiFile
-import org.jetbrains.uast.*
-import org.jetbrains.uast.visitor.UastVisitor
+import org.jetbrains.uast.UFile
+import org.jetbrains.uast.toUElement
 
 class EntryPointInspection : AbstractBaseUastLocalInspectionTool(UFile::class.java) {
     //private val rules = Helper.loadRules(Config.PATH, Config.TYPE_CONSTRUCTOR)
@@ -17,37 +19,7 @@ class EntryPointInspection : AbstractBaseUastLocalInspectionTool(UFile::class.ja
         val uFile = file.toUElement(UFile::class.java) ?: return ProblemDescriptor.EMPTY_ARRAY
         val issueList = arrayListOf<ProblemDescriptor>()
 
-        uFile.accept(object : UastVisitor {
-            override fun visitElement(node: UElement): Boolean {
-                return false
-            }
-
-            override fun visitExpression(node: UExpression): Boolean {
-                if (node::class.simpleName?.contains("UCallExpression") ?: false) {
-                    node.accept(CallExpressionVisitor(manager, issueList, isOnTheFly))
-                    if (node is UCallExpression) {
-                        val args = node.valueArguments
-                        var i = 0;
-                        for (arg in args) {
-                            if (arg is USimpleNameReferenceExpression) {
-                                val resolvedVar = arg.resolve()
-                                issueList.add(
-                                    manager.createProblemDescriptor(
-                                        resolvedVar ?: continue,
-                                        "tracer $i ${resolvedVar}: ${node.asSourceString()}",
-                                        isOnTheFly,
-                                        LocalQuickFix.EMPTY_ARRAY,
-                                        ProblemHighlightType.WARNING,
-                                    )
-                                )
-                            }
-                            i += 1
-                        }
-                    }
-                }
-                return false
-            }
-        })
+        uFile.accept(CallExpressionVisitor(manager, issueList, isOnTheFly))
 
         return issueList.toTypedArray()
     }
--- src/main/kotlin/com/github/binhlecong/androidscanner/visitors/CallExpressionVisitor.kt
+++ src/main/kotlin/com/github/binhlecong/androidscanner/visitors/CallExpressionVisitor.kt
@@ -4,8 +4,10 @@ import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
+import org.jetbrains.uast.UCallExpression
 import org.jetbrains.uast.UElement
-import org.jetbrains.uast.tryResolve
+import org.jetbrains.uast.UExpression
+import org.jetbrains.uast.USimpleNameReferenceExpression
 import org.jetbrains.uast.visitor.UastVisitor
 
 class CallExpressionVisitor(
@@ -14,54 +16,92 @@ class CallExpressionVisitor(
     private val isOnTheFly: Boolean,
 ) : UastVisitor {
     override fun visitElement(node: UElement): Boolean {
-        val sourcePsi = node.sourcePsi ?: return false
-
-        if (node::class.simpleName?.contains("USimpleNameReferenceExpression") ?: false) {
-            val refDeclaration = node.tryResolve()
-            if (refDeclaration != null) {
-                issueList.add(
-                    manager.createProblemDescriptor(
-                        refDeclaration,
-                        "${refDeclaration::class.simpleName}: ${refDeclaration.text}",
-                        isOnTheFly,
-                        LocalQuickFix.EMPTY_ARRAY,
-                        ProblemHighlightType.WARNING,
-                    )
-                )
-            }
-        }
-        if (node::class.simpleName?.contains("UCallExpression") ?: false) {
-            issueList.add(
-                manager.createProblemDescriptor(
-                    sourcePsi,
-                    "${node::class.simpleName}: ${node.asSourceString()}",
-                    isOnTheFly,
-                    LocalQuickFix.EMPTY_ARRAY,
-                    ProblemHighlightType.WARNING,
-                )
-            )
-        }
-        if (node::class.simpleName?.contains("ULiteralExpression") ?: false) {
-            issueList.add(
-                manager.createProblemDescriptor(
-                    sourcePsi,
-                    "${node::class.simpleName}: ${node.asSourceString()}",
-                    isOnTheFly,
-                    LocalQuickFix.EMPTY_ARRAY,
-                    ProblemHighlightType.WARNING,
-                )
-            )
-        }
-        // todo: use this to check expr class name
-//        issueList.add(
-//            manager.createProblemDescriptor(
-//                sourcePsi,
-//                "${node::class.simpleName}: ${node.asSourceString()}",
-//                isOnTheFly,
-//                LocalQuickFix.EMPTY_ARRAY,
-//                ProblemHighlightType.WARNING,
-//            )
-//        )
         return false
     }
+
+    override fun visitExpression(node: UExpression): Boolean {
+        if (node::class.simpleName?.contains("UCallExpression") ?: false) {
+            val sourcePsi = node.sourcePsi ?: return false
+            issueList.add(
+                manager.createProblemDescriptor(
+                    sourcePsi,
+                    "${node::class.simpleName}: ${node.asSourceString()}",
+                    isOnTheFly,
+                    LocalQuickFix.EMPTY_ARRAY,
+                    ProblemHighlightType.WARNING,
+                )
+            )
+            if (node is UCallExpression) {
+                val args = node.valueArguments
+                var i = 0;
+                for (arg in args) {
+                    if (arg is USimpleNameReferenceExpression) {
+                        val resolvedVar = arg.resolve()
+                        issueList.add(
+                            manager.createProblemDescriptor(
+                                resolvedVar ?: continue,
+                                "Traced ref $i ${resolvedVar}: ${node.asSourceString()}",
+                                isOnTheFly,
+                                LocalQuickFix.EMPTY_ARRAY,
+                                ProblemHighlightType.WARNING,
+                            )
+                        )
+                    }
+                    i += 1
+                }
+            }
+        }
+        return false
+    }
+//    override fun visitElement(node: UElement): Boolean {
+//        val sourcePsi = node.sourcePsi ?: return false
+//
+//        if (node::class.simpleName?.contains("USimpleNameReferenceExpression") ?: false) {
+//            val refDeclaration = node.tryResolve()
+//            if (refDeclaration != null) {
+//                issueList.add(
+//                    manager.createProblemDescriptor(
+//                        refDeclaration,
+//                        "${refDeclaration::class.simpleName}: ${refDeclaration.text}",
+//                        isOnTheFly,
+//                        LocalQuickFix.EMPTY_ARRAY,
+//                        ProblemHighlightType.WARNING,
+//                    )
+//                )
+//            }
+//        }
+//        if (node::class.simpleName?.contains("UCallExpression") ?: false) {
+//            issueList.add(
+//                manager.createProblemDescriptor(
+//                    sourcePsi,
+//                    "${node::class.simpleName}: ${node.asSourceString()}",
+//                    isOnTheFly,
+//                    LocalQuickFix.EMPTY_ARRAY,
+//                    ProblemHighlightType.WARNING,
+//                )
+//            )
+//        }
+//        if (node::class.simpleName?.contains("ULiteralExpression") ?: false) {
+//            issueList.add(
+//                manager.createProblemDescriptor(
+//                    sourcePsi,
+//                    "${node::class.simpleName}: ${node.asSourceString()}",
+//                    isOnTheFly,
+//                    LocalQuickFix.EMPTY_ARRAY,
+//                    ProblemHighlightType.WARNING,
+//                )
+//            )
+//        }
+//        // todo: use this to check expr class name
+////        issueList.add(
+////            manager.createProblemDescriptor(
+////                sourcePsi,
+////                "${node::class.simpleName}: ${node.asSourceString()}",
+////                isOnTheFly,
+////                LocalQuickFix.EMPTY_ARRAY,
+////                ProblemHighlightType.WARNING,
+////            )
+////        )
+//        return false
+//    }
 }
