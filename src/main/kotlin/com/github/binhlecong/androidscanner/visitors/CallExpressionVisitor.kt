af99e0bf08d6b006be9fbb9ac87db1d17abd6b5b
binhlecong <congbinh21012001@gmail.com>
2022-11-02 12:57:26 +0700

    demo get expression and get referenced var with reduce duplication

--- src/main/kotlin/com/github/binhlecong/androidscanner/visitors/CallExpressionVisitor.kt
+++ src/main/kotlin/com/github/binhlecong/androidscanner/visitors/CallExpressionVisitor.kt
@@ -22,7 +22,7 @@ class CallExpressionVisitor(
                 issueList.add(
                     manager.createProblemDescriptor(
                         refDeclaration,
-                        "${node::class.simpleName}: ${refDeclaration.text}",
+                        "${refDeclaration::class.simpleName}: ${refDeclaration.text}",
                         isOnTheFly,
                         LocalQuickFix.EMPTY_ARRAY,
                         ProblemHighlightType.WARNING,
@@ -41,6 +41,18 @@ class CallExpressionVisitor(
                 )
             )
         }
+        if (node::class.simpleName?.contains("ULiteralExpression") ?: false) {
+            issueList.add(
+                manager.createProblemDescriptor(
+                    sourcePsi,
+                    "${node::class.simpleName}: ${node.asSourceString()}",
+                    isOnTheFly,
+                    LocalQuickFix.EMPTY_ARRAY,
+                    ProblemHighlightType.WARNING,
+                )
+            )
+        }
+        // todo: use this to check expr class name
 //        issueList.add(
 //            manager.createProblemDescriptor(
 //                sourcePsi,
@@ -52,94 +64,4 @@ class CallExpressionVisitor(
 //        )
         return false
     }
-//
-//    override fun visitCallableReferenceExpression(node: UCallableReferenceExpression): Boolean {
-//        val sourcePsi = node.sourcePsi ?: return false
-//
-//        issueList.add(
-//            manager.createProblemDescriptor(
-//                sourcePsi,
-//                "CallableRefExpr: ${node::class.simpleName}: ${node.asSourceString()}",
-//                isOnTheFly,
-//                LocalQuickFix.EMPTY_ARRAY,
-//                ProblemHighlightType.WARNING,
-//            )
-//        )
-//        return false
-//    }
-//
-//    override fun visitSimpleNameReferenceExpression(node: USimpleNameReferenceExpression): Boolean {
-//        val sourcePsi = node.sourcePsi ?: return false
-//
-//        issueList.add(
-//            manager.createProblemDescriptor(
-//                sourcePsi,
-//                "SimpleNameRefExpr: ${node::class.simpleName}: ${node.asSourceString()}",
-//                isOnTheFly,
-//                LocalQuickFix.EMPTY_ARRAY,
-//                ProblemHighlightType.WARNING,
-//            )
-//        )
-//        return false
-//    }
-//
-//    override fun visitQualifiedReferenceExpression(node: UQualifiedReferenceExpression): Boolean {
-//        val sourcePsi = node.sourcePsi ?: return false
-//
-//        issueList.add(
-//            manager.createProblemDescriptor(
-//                sourcePsi,
-//                "QualifiedRefExpr: ${node::class.simpleName}: ${node.asSourceString()}",
-//                isOnTheFly,
-//                LocalQuickFix.EMPTY_ARRAY,
-//                ProblemHighlightType.WARNING,
-//            )
-//        )
-//        return false
-//    }
-//
-//    override fun visitTypeReferenceExpression(node: UTypeReferenceExpression): Boolean {
-//        val sourcePsi = node.sourcePsi ?: return false
-//
-//        issueList.add(
-//            manager.createProblemDescriptor(
-//                sourcePsi,
-//                "TypeRefExpr: ${node::class.simpleName}: ${node.asSourceString()}",
-//                isOnTheFly,
-//                LocalQuickFix.EMPTY_ARRAY,
-//                ProblemHighlightType.WARNING,
-//            )
-//        )
-//        return false
-//    }
-//
-//    override fun visitLiteralExpression(node: ULiteralExpression): Boolean {
-//        val sourcePsi = node.sourcePsi ?: return false
-//
-//        issueList.add(
-//            manager.createProblemDescriptor(
-//                sourcePsi,
-//                "LiteralExpr: ${node::class.simpleName}: ${node.asSourceString()}",
-//                isOnTheFly,
-//                LocalQuickFix.EMPTY_ARRAY,
-//                ProblemHighlightType.WARNING,
-//            )
-//        )
-//        return false
-//    }
-//
-//    override fun visitMethod(node: UMethod): Boolean {
-//        val sourcePsi = node.sourcePsi ?: return false
-//
-//        issueList.add(
-//            manager.createProblemDescriptor(
-//                sourcePsi,
-//                "Method: ${node::class.simpleName}: ${node.asSourceString()}",
-//                isOnTheFly,
-//                LocalQuickFix.EMPTY_ARRAY,
-//                ProblemHighlightType.WARNING,
-//            )
-//        )
-//        return false
-//    }
 }
