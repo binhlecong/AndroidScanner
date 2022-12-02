6616d252b31503839ea1dc10ffc5341248c2b62b
binhlecong <congbinh21012001@gmail.com>
2022-12-02 23:25:21 +0700

    init UI of rule manager

---
+++ src/main/kotlin/com/github/binhlecong/androidscanner/actions/RulesManagerForm.java
@@ -0,0 +1,15 @@
+package com.github.binhlecong.androidscanner.actions;
+
+import javax.swing.*;
+
+public class RulesManagerForm {
+    private JComboBox selectLangDropdown;
+    private JLabel selectLangLabel;
+    private JScrollPane rulesScrollView;
+    private JScrollPane inspectorScrollView;
+    private JTable table1;
+    private JPanel buttonsRow;
+    private JButton saveButton;
+    private JButton cancelButton;
+    private JButton addRuleButton;
+}
