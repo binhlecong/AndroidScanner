0ae39132b5b9491e24c37ee26ddb247e01c07675
binhlecong <congbinh21012001@gmail.com>
2023-02-06 09:11:37 +0700

    change logo size

--- src/main/kotlin/icons/MyIcons.kt
+++ src/main/kotlin/icons/MyIcons.kt
@@ -4,14 +4,14 @@ import com.intellij.openapi.util.IconLoader
 
 object MyIcons {
     @JvmField
-    val ErrorIcon = IconLoader.getIcon("/icons/errorIcon.svg", javaClass)
+    val ErrorIcon = IconLoader.getIcon("/icons/Error Icon 12x12.svg", javaClass)
 
     @JvmField
-    val InfoIcon = IconLoader.getIcon("/icons/infoIcon.svg", javaClass)
+    val InfoIcon = IconLoader.getIcon("/icons/Info Icon 12x12.svg", javaClass)
 
     @JvmField
-    val PluginIcon = IconLoader.getIcon("/icons/pluginIcon.svg", javaClass)
+    val PluginIcon = IconLoader.getIcon("/icons/Main Icon 16x16.svg", javaClass)
 
     @JvmField
-    val WarningIcon = IconLoader.getIcon("/icons/warningIcon.svg", javaClass)
+    val WarningIcon = IconLoader.getIcon("/icons/Warning Icon 12x12.svg", javaClass)
 }
